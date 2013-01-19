457db1b0045f5e1726f9e32894f0a4c49e8fba8b
Tigggger <nzbnation@mail.com>
2013-01-18 16:03:30 -0800

    Create backfill_inc.bat

---
+++ backfill_inc.bat
@@ -0,0 +1,17 @@
+:Top
+
+CD..
+php.exe backfill_inc.php
+CD..
+CD testing
+php.exe update_parsing.php
+php.exe removespecial.php
+php.exe update_cleanup.php
+CD..
+CD update_scripts
+php.exe optimise_db.php
+CD win_scripts
+
+Sleep 15
+
+GOTO TOP
