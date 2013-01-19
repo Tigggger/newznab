b55321290869dcbac27a18f2a5fefdd066b90113
Tigggger <nzbnation@mail.com>
2013-01-19 15:21:10 +0000

    Update backfill_inc.bat

--- backfill_inc.bat
+++ backfill_inc.bat
@@ -5,8 +5,8 @@ php.exe backfill_inc.php
 CD..
 CD testing
 php.exe update_parsing.php
-php.exe removespecial.php
-php.exe update_cleanup.php
+REM php.exe removespecial.php
+REM php.exe update_cleanup.php
 CD..
 CD update_scripts
 php.exe optimise_db.php
